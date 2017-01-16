bd769683eeb93de497937dc8c995800c3958c51d
Sajan64 <thapa.szn64@gmail.com>
2017-01-16 09:15:11 +0545

    Selenium example file changes

--- src/main/java/Selenium2Example.java
+++ src/main/java/Selenium2Example.java
@@ -10,21 +10,30 @@ public class Selenium2Example  {
         // Create a new instance of the Firefox driver
         // Notice that the remainder of the code relies on the interface, 
         // not the implementation.
+        System.setProperty("webdriver.gecko.driver","F:\\geckodriver.exe");
+
         WebDriver driver = new FirefoxDriver();
 
         // And now use this to visit Google
-        driver.get("http://www.google.com");
+        driver.get("http://localhost:81/wordpress/wp-admin");
         // Alternatively the same thing can be done like this
         // driver.navigate().to("http://www.google.com");
 
         // Find the text input element by its name
-        WebElement element = driver.findElement(By.name("q"));
+        WebElement element1 = driver.findElement(By.id("user_login"));
 
         // Enter something to search for
-        element.sendKeys("Cheese!");
+        element1.sendKeys("admin");
+
+        WebElement element2 = driver.findElement(By.id("user_pass"));
+
+        // Enter something to search for
+        element2.sendKeys("admin");
 
         // Now submit the form. WebDriver will find the form for us from the element
-        element.submit();
+        WebElement element3 = driver.findElement(By.id("wp-submit"));
+
+        element3.submit();
 
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
@@ -41,6 +50,5 @@ public class Selenium2Example  {
         System.out.println("Page title is: " + driver.getTitle());
 
         //Close the browser
-        driver.quit();
     }
 }
