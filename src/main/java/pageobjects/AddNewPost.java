ac9129436ff5ebe7bf0bb72b5eb78efeb3d6650c
Kusum <jeneliashrestha94@gmail.com>
2017-01-18 09:31:46 +0545

    adding Add new post pageobject

--- src/main/java/pageobjects/AddNewPost.java
+++ src/main/java/pageobjects/AddNewPost.java
@@ -29,7 +29,7 @@ public class AddNewPost {
 
     }
 
-    public AddNewPost() {
+    public doAddNewPost() {
 
       title.sendKeys("chandagiri is beautiful");
       paragraph.sendKeys("chandagiri is located in thankot .you can view changagiri hills by cable car");
