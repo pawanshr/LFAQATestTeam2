6060104cbc47eccc7263057d333e31db580fa8ec
Sajan64 <thapa.szn64@gmail.com>
2017-01-20 09:06:36 +0545

    addnew page modified

--- src/main/java/pageobjects/AddNewPage.java
+++ src/main/java/pageobjects/AddNewPage.java
@@ -4,34 +4,31 @@ package pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * Created by thapa on 1/18/2017.
  */
 public class AddNewPage {
 
-//    System.setproperty("webdriver.firefox.marionette","F:\\LeapFrog\\geckodriver.exe");
-
     WebDriver driver;
-    WebElement addpage = driver.findElement(By.linkText("Add additional pages"));
-    WebElement addtitle = driver.findElement(By.id("title"));
-    WebElement pagecontent = driver.findElement(By.id("tinymce"));
-    WebElement publishpage = driver.findElement(By.id("publish"));
-    WebElement preview = driver.findElement(By.id("post-preview"));
+    @FindBy (id = "title")
+    WebElement addtitle;
+    @FindBy (id = "tinymce")
+    WebElement pagecontent;
+    @FindBy (id = "publish")
+    WebElement publishpage;
+    @FindBy (id = "post-preview")
+    WebElement preview;
+
+
 
 
     public AddNewPage(WebDriver driver){
         this.driver = driver;
     }
 
-    public void doaddpage(){
-        addpage.click();
-    }
-
-    public void doAddtitle() {
-        addtitle.sendKeys("QA Class");
-
-    }
+    public void doAddtitle() { addtitle.sendKeys("QA Class"); }
 
     public void addpagecontent(){
         pagecontent.sendKeys("A B C D X Y Z");
