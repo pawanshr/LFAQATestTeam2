9683e4d0e5a08aa35be2e2485342e4f25de8d2d5
Kusum <jeneliashrestha94@gmail.com>
2017-01-23 07:24:44 +0545

    new post test created

---
+++ src/test/java/tests/NewPostTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import pageobjects.Login;
+
+/**
+ * Created by Genelia on 20/01/2017.
+ */
+public class NewPostTest {
+
+    @Test
+    public void testThatPostWorks(){
+        System.setProperty("webdriver.firefox.marionette","C:\\LeapFrog\\geckodriver.exe");
+
+        WebDriver driver=new FirefoxDriver();
+        driver.get("http://localhost/wordpress/wp-admin");
+
+        Login login = new Login(driver);
+        PageFactory.initElements(driver,login);
+        login.setUsername();
+        login.setPassword();
+        login.setLogin();
+
+
+
+
+
+    }
+}
