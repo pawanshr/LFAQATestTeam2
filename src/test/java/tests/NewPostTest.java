220e14318e4ec489c7e67f5c4199c4aabb3fca8f
Kusum <jeneliashrestha94@gmail.com>
2017-01-24 22:03:30 +0545

    new post test modified

--- src/test/java/tests/NewPostTest.java
+++ src/test/java/tests/NewPostTest.java
@@ -22,17 +22,21 @@ public class NewPostTest extends BaseTest {
 
 
         Dashboard dashboard=new Dashboard(driver);
+        PageFactory.initElements(driver,dashboard);
         dashboard.gotoPages();
-        dashboard.gotoAddNewPage();
+        dashboard.gotoAddNewPages();
+
         dashboard.gotoPosts();
         dashboard.gotoAddNewPost();
 
         AddNewPage addNewPage=new AddNewPage(driver);
-        addNewPage.doAddtitle("pagetitle");
-        addNewPage.addpagecontent("pagecontent");
+        PageFactory.initElements(driver,addNewPage);
+        addNewPage.doAddtitle("this is the title page");
+        addNewPage.addpagecontent("this is where my content goes");
         addNewPage.dopublishpage();
 
          AddNewPost addNewPost=new AddNewPost(driver);
+        PageFactory.initElements(driver,addNewPost);
         addNewPost.dotitle("title");
         addNewPost.paragraph("paragraph");
         addNewPost.dopublish();
