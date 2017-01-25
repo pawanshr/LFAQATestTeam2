a7ea997f6f170295ce878019363f23cb2522f727
Kusum <jeneliashrestha94@gmail.com>
2017-01-25 07:47:53 +0545

    new post test modified

--- src/test/java/tests/NewPostTest.java
+++ src/test/java/tests/NewPostTest.java
@@ -23,19 +23,11 @@ public class NewPostTest extends BaseTest {
 
         Dashboard dashboard=new Dashboard(driver);
         PageFactory.initElements(driver,dashboard);
-        dashboard.gotoPages();
-        dashboard.gotoAddNewPages();
 
         dashboard.gotoPosts();
         dashboard.gotoAddNewPost();
 
-        AddNewPage addNewPage=new AddNewPage(driver);
-        PageFactory.initElements(driver,addNewPage);
-        addNewPage.doAddtitle("this is the title page");
-        addNewPage.addpagecontent("this is where my content goes");
-        addNewPage.dopublishpage();
-
-         AddNewPost addNewPost=new AddNewPost(driver);
+        AddNewPost addNewPost=new AddNewPost(driver);
         PageFactory.initElements(driver,addNewPost);
         addNewPost.dotitle("title");
         addNewPost.paragraph("paragraph");
@@ -43,15 +35,5 @@ public class NewPostTest extends BaseTest {
 
 
     }
-
-
-
-
-
-
-
-
-
-
 
 }
