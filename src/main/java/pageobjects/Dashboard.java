b131f9fdc121b30f53c899e7c157420020bd35ac
Bikram <bikthapa41@gmail.com>
2017-01-25 11:08:10 +0545

    dashboard test done

--- src/main/java/pageobjects/Dashboard.java
+++ src/main/java/pageobjects/Dashboard.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 /**
  * Created by superuser on 1/18/2017.
@@ -33,7 +34,9 @@ public class Dashboard {
     WebElement addNewUserLink;
 
     public Dashboard(WebDriver driver) {
+
         this.driver = driver;
+        PageFactory.initElements(driver,this);
     }
 
     public void gotohomeLink() {
