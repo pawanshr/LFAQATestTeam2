cca7d0ba467bfe22356cbe628450a8e251d175b7
Rajnish Pradhan <Rajnish Pradhan>
2017-01-27 09:14:40 +0545

    files moved to package

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,19 +1,6 @@
 package tests;
 
-import org.apache.bcel.generic.NEW;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import pageobjects.AddNewUser;
-import org.openqa.selenium.support.PageFactory;
-import pageobjects.AddNewPage;
-import pageobjects.AddNewPost;
-import pageobjects.Dashboard;
-import pageobjects.Login;
-
-import java.sql.Driver;
 
 /**
  * Created by Ujjwal on 1/18/2017.
