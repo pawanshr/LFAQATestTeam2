b5bc67a491891a3021721e95ca3675c9a9730414
Sajan64 <thapa.szn64@gmail.com>
2017-01-30 07:43:51 +0545

    assert functionality added

---
+++ src/main/java/pageobjects/page/DeletePage.java
@@ -0,0 +1,15 @@
+package pageobjects.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * Created by thapa on 1/29/2017.
+ */
+public class DeletePage {
+
+    WebDriver driver;
+    WebElement checkbox;
+
+}
