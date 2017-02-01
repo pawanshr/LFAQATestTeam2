add09db5e009f48dddae4df2a24c0ceb3d6e29fb
Pawan Krishna Shrestha <i80921@d2hs.com>
2017-02-01 08:38:47 +0545

    added IE and Chrome Driver

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,7 +3,11 @@ package tests;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 import pageobjects.login.Login;
 import java.util.concurrent.TimeUnit;
@@ -18,8 +22,19 @@ public class BaseTest {
 
     @Before
     public void setup(){
-        System.setProperty("webdriver.firefox.marionette","C:\\Leapfrog\\geckodriver.exe");
-        driver=new FirefoxDriver();
+        //System.setProperty("webdriver.firefox.marionette","C:\\Leapfrog\\geckodriver.exe");
+        //System.setProperty("webdriver.chrome.driver","C:\\Leapfrog\\chromedriver.exe");
+        System.setProperty("webdriver.ie.driver","C:\\Leapfrog\\IEDriverServer.exe");
+
+        //driver=new FirefoxDriver();
+        //driver = new ChromeDriver();
+        InternetExplorerDriverService service = new InternetExplorerDriverService.Builder().build();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
+        capabilities.setCapability(InternetExplorerDriver.ENABLE_ELEMENT_CACHE_CLEANUP,true);
+        capabilities.setCapability("enablePersistentHover", false);
+        driver = new InternetExplorerDriver(service, capabilities);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://localhost/wordpress/wp-admin");
