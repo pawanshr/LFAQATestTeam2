c1d9a8754e615d2b8ded369fee1bbd0db1a5c2a0
Bikram <bikthapa41@gmail.com>
2017-02-06 08:34:55 +0545

    Settings test added

---
+++ src/main/java/pageobjects/settings/Settings.java
@@ -0,0 +1,101 @@
+package pageobjects.settings;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ * Created by superuser on 2/4/2017.
+ */
+public class Settings {
+    WebDriver driver;
+
+    @FindBy(id= "blogname")
+    WebElement sitetitle;
+    @FindBy(id= "blogdescription")
+    WebElement tagline;
+    @FindBy(id = "siteurl")
+    WebElement wpaddressurl;
+    @FindBy(id = "home")
+    WebElement siteaddressurl;
+    @FindBy(id = "admin_email")
+    WebElement emailaddress;
+    @FindBy(id ="users_can_register")
+    WebElement membership;
+    @FindBy(id ="default_role")
+    WebElement role;
+    @FindBy(id ="timezone_string")
+    WebElement timezone;
+    @FindBy(xpath = ".//*[@id='wpbody-content']/div[3]/form/table/tbody/tr[9]/td/fieldset/label[1]/span")
+    WebElement dateformat;
+    @FindBy(xpath =".//*[@id='wpbody-content']/div[3]/form/table/tbody/tr[10]/td/fieldset/label[1]/input")
+    WebElement timeformat;
+    @FindBy(id="start_of_week")
+    WebElement week;
+    @FindBy(id="WPLANG")
+    WebElement sitelanguage;
+    @FindBy(id="submit")
+    WebElement savechanges;
+
+    public Settings(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+
+    public void setSitetitle(String sitetitletext){
+        sitetitle.sendKeys(sitetitletext);
+    }
+
+    public void setTagline(String taglinetext){
+        tagline.sendKeys(taglinetext);
+    }
+
+    public void setWpaddressurl(String wpaddressurltext){
+        wpaddressurl.sendKeys(wpaddressurltext);
+    }
+
+    public void setSiteaddressurl(String siteaddressurltext){
+        siteaddressurl.sendKeys(siteaddressurltext);
+    }
+
+    public void setEmailaddress(String emailaddresstext){
+        emailaddress.sendKeys(emailaddresstext);
+    }
+
+    public void setMembership(){
+        membership.click();
+    }
+
+    public void setRole(String roletext){
+        Select dropdown = new Select(role);
+        dropdown.selectByVisibleText(roletext);
+    }
+
+    public void setTimezone(String timezonetext){
+        Select dropdown = new Select(timezone);
+        dropdown.selectByVisibleText(timezonetext);
+    }
+
+    public void setDateformat(){
+        dateformat.click();
+    }
+
+    public void setTimeformat(){
+        timeformat.click();
+    }
+
+    public void setWeek(String weektext){
+        Select dropdown = new Select(week);
+        dropdown.selectByVisibleText(weektext);
+    }
+
+    public void setSitelanguage(String sitelanguagetext){
+        Select dropdown = new Select(sitelanguage);
+        dropdown.selectByVisibleText(sitelanguagetext);
+    }
+
+    public void setSavechanges(){
+        savechanges.click();
+    }
+}
---
+++ src/test/java/tests/settingsTest/SettingsTest.java
@@ -0,0 +1,33 @@
+package tests.settingsTest;
+
+import org.junit.Test;
+import pageobjects.dashboard.Dashboard;
+import pageobjects.settings.Settings;
+import tests.BaseTestLogin;
+
+/**
+ * Created by superuser on 2/4/2017.
+ */
+public class SettingsTest extends BaseTestLogin {
+    @Test
+    public void testThatSettingsWorks(){
+        Dashboard dashboard = new Dashboard(driver);
+        dashboard.gotoSettings();
+
+        Settings settings = new Settings(driver);
+        settings.setSitetitle("Career Academy");
+        settings.setTagline("this is my project");
+        settings.setWpaddressurl("http://localhost/wordpress");
+        settings.setSiteaddressurl("http://localhost/wordpress");
+        settings.setEmailaddress("bikthapa41@gmail.com");
+        settings.setMembership();
+        settings.setRole("Administrator");
+        settings.setTimezone("UTC+5:45");
+        settings.setDateformat();
+        settings.setTimeformat();
+        settings.setWeek("Sunday");
+        settings.setSitelanguage("Deutsch");
+        settings.setSavechanges();
+    }
+}
+
